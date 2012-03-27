99e0d856edccbbcbad24b8373149b1f28fa32485
jordan.hatch <jordan.hatch@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-27 23:01:07 +0000

    git-svn-id: https://plugins.svn.wordpress.org/infusionsoft-web-tracker/trunk@524521 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- infusionsoft-tracker_import_admin.php
+++ infusionsoft-tracker_import_admin.php
@@ -46,21 +46,33 @@ input.infusionWebTrackerInput{
 			
 		}
 	?>
+<style>
+    .panel{
+	background: #f6f6f6;
+	width: 300px;
+	height: 300px;
+	display: block;
+	float: left;
+	border: 2px groove #f6f6f6;
+    }
+    #about{
+	width: 600px;
 	
+    }
+</style>
 <div class="wrap">
     <?php    echo "<h2>" . __( 'Infusionsoft Analytics for WordPress', 'infusionsoft_tracker_trdom' ) . "</h2>"; ?>
     <div class="panel" id="apiPanel">
 	<h3>Enter Your API Details</h3>		
 	<form name="infusionsoft_tracker_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
 	    <input type="hidden" name="infusionsoft_tracker_hidden" value="Y">
-	    <table class='options' style="width:600px">
-	    <tr><td width=30%><p><?php _e("Infusionsoft Application Name: " ); ?></td><td><input class='infusionsoft_tracker' type="text" name="infusionsoft_tracker_appName" value="<?php echo $appName; ?>" size="16"><?php _e(".infusionsoft.com" ); ?></p></td></tr>
-	    <tr><td width=30%><p><?php _e("Infusionsoft API Key: " ); ?></td><td><input class='infusionsoft_trackerInput' type="password" name="infusionsoft_tracker_apiKey" value="<?php echo $apiKey; ?>" size="32"><?php _e("<a href='http://kb.infusionsoft.com/article/AA-00442/0/How-do-I-enable-the-Infusionsoft-API-and-generate-an-API-Key.html'>Learn How to Get It</a>" ); ?></p></td></tr>
 	    
-	    <tr><td colspan=2><p class="submit">
+	    <p><?php _e("Infusionsoft Application Name: " ); ?></p>
+	    <input class='infusionsoft_tracker' type="text" name="infusionsoft_tracker_appName" value="<?php echo $appName; ?>" size="16"><?php _e(".infusionsoft.com" ); ?>
+	    <p><?php _e("Infusionsoft API Key: " ); ?></p>
+	    <input class='infusionsoft_trackerInput' type="password" name="infusionsoft_tracker_apiKey" value="<?php echo $apiKey; ?>" size="32"><br />
 	    <input type="submit" name="Submit" value="<?php _e('Save', 'infusionsoft_tracker_trdom' ) ?>" />
-	    </p></td></tr>
-	    </table>
+	
 	</form>
     </div>
     <div class="panel" id="video">
